ae30f0fb15eabba6c18c1bc21c55cdfa0517d0af
greenstork <dmanelski@salesforce.com>
2013-02-26 10:29:21 -0800

    updated to use dynamic heroku process port

--- app.js
+++ app.js
@@ -50,7 +50,7 @@ app.delete('/api/post/:id', api.deletePost);
 app.get('*', routes.index);
 
 // Start server
-
-app.listen(3000, function(){
+var port = process.env.PORT || 3000;
+app.listen(port, function(){
   console.log("Express server listening on port %d in %s mode", app.address().port, app.settings.env);
 });
